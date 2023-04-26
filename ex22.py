725367e409df1cb0035908f6ef9ca315fd08faf5
alexiatak <113921367+alexiatak@users.noreply.github.com>
2023-04-26 13:26:10 +0300

    Update ex22.py

--- ex22.py
+++ ex22.py
@@ -7,11 +7,15 @@ ffile = fits.open('noised.fits')
 img = ffile[0].data
 transf_img= fft.fft2(img)
 
+img = numpy.abs(transf_img)
+hdu = fits.PrimaryHDU(data=img)
+fits.HDUList([hdu]).writeto("fft.fits", overwrite=True)
 
-#d1 = (804, 881)
-#d2 = (834, 911)
-#transf_img[d1] = numpy.median(transf_img[d1[0] - 1:d1[0] + 1, d1[1] - 1:d1[1] + 1])
-#transf_img[d2] = numpy.median(transf_img[d2[0] - 1:d2[0] + 1, d2[1] - 1:d2[1] + 1])
+
+d1 = (15,15)
+d2 = (1623,1778)
+transf_img[d1] = numpy.median(transf_img[d1[0] - 1:d1[0] + 1, d1[1] - 1:d1[1] + 1])
+transf_img[d2] = numpy.median(transf_img[d2[0] - 1:d2[0] + 1, d2[1] - 1:d2[1] + 1])
 
 
 img = numpy.abs(fft.ifft2(transf_img))
